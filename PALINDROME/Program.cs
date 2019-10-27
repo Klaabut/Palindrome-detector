eaa9b8e9b15be57a05dff6ba4ebae473d8a1b8d6
Klaabut <54986407+Klaabut@users.noreply.github.com>
2019-10-27 20:16:38 +0200

    microwave noises

---
+++ PALINDROME/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PALINDROME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            string reversed;
+            char[] Palin = name.ToCharArray();
+
+            Array.Reverse(Palin);
+            reversed = new string(Palin);
+
+            if (name == reversed)
+            {
+                Console.WriteLine("Your name is a palindrome. ");
+            }
+            else
+            {
+                Console.WriteLine("Your name is not a palindrome.");
+            }
+
+            
+            
+
+
+
+            Console.ReadLine();
+        }
+
+      
+
+
+
+        
+    }
+
+}
